de21df2ebaa990c56f32c993f6d2c35255548cc8
notReally47 <112917927+notReally47@users.noreply.github.com>
2022-10-19 19:31:39 +0800

    v2.0
    Commented splash screen state. Jumps to base game for quick tests

--- Src/main.c
+++ Src/main.c
@@ -1,9 +1,12 @@
 #include "cprocessing.h"
 #include "splashscreen.h"
+#include "basegame.h"
 
 int main(void)
 {
-	CP_Engine_SetNextGameState(splash_screen_init,splash_screen_update,splash_screen_exit);
+	//CP_Engine_SetNextGameState(splash_screen_init,splash_screen_update,splash_screen_exit);
+	// skip to basegame
+	CP_Engine_SetNextGameState(base_Init,base_Update,base_Exit);
 	CP_Engine_Run();
 	return 0;
 }
