60c2b2c756f224754e7c3887e0fa71c3b4e886b9
notReally47 <112917927+notReally47@users.noreply.github.com>
2022-10-19 19:34:57 +0800

    v2.0
    - Removed unnecessary functions
    - Swap key press to key down (cause carpal tunnel)
    - Added delay to key down

--- Src/utils.c
+++ Src/utils.c
@@ -1,5 +1,4 @@
 #include "cprocessing.h"
-#include "math.h"
 #include "structs.h"
 #include "defines.h"
 #include "utils.h"
@@ -14,36 +13,30 @@ int IsAreaClicked(float area_center_x, float area_center_y, float area_width, fl
 	float y2 = area_center_y + area_height * 0.5f;
 	return ((click_x<x2&& click_x>x1 && click_y<y2&& click_y>y1) ? 1 : 0); // return 1 if the mouse is within the rectangle
 }
-/*
-int IsCircleClicked(float circle_center_x, float circle_center_y, float diameter, float click_x, float click_y)
-{
-	float radius = diameter*0.5f; // finding radius of the circle
-	float distmouse = CP_Math_Distance(circle_center_x,circle_center_y,click_x,click_y); // finding distance from mouse click to the center of the circle
-	return ((distmouse<radius)?1:0); // return 1 if distance from the mouse is less than the radius
-}
-*/
-CP_Vector AngleToVector(float radian_angle)
-{	// for movement of sprites ?
-	float x = (float)cos(CP_Math_Radians(radian_angle)); // finding the x value of a vector by calculating the cosine of the radian angle
-	float y = (float)sin(CP_Math_Radians(radian_angle)); // finding the y value of a vector by calculating the sine of the radian angle
-	return (CP_Vector_Scale(CP_Vector_Set(x, y), 9.f)); // return the vector with a scalar
-}
 
 /*
 * getDirection - returns the direction by input.
 */
 int getDirection(void) {
-	if (CP_Input_KeyTriggered(KEY_W))
-		return SOKOBAN_UP;
+	static float delay = 0.f;
+	delay += CP_System_GetDt();
 
-	else if (CP_Input_KeyTriggered(KEY_A))
-		return SOKOBAN_LEFT;
+	if (delay >= 0.08f) {
+		delay = 0.f;
+		if (CP_Input_KeyDown(KEY_W))
+			return SOKOBAN_UP;
 
-	else if (CP_Input_KeyTriggered(KEY_S))
-		return SOKOBAN_DOWN;
+		else if (CP_Input_KeyDown(KEY_A))
+			return SOKOBAN_LEFT;
 
-	else if (CP_Input_KeyTriggered(KEY_D))
-		return SOKOBAN_RIGHT;
+		else if (CP_Input_KeyDown(KEY_S))
+			return SOKOBAN_DOWN;
+
+		else if (CP_Input_KeyDown(KEY_D))
+			return SOKOBAN_RIGHT;
+		
+	}
+	
 
 	return SOKOBAN_IDLE;
 }
