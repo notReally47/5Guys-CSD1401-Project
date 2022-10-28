b524079eb5b54af2605c90c09e3f0be74bf619b9
notReally47 <112917927+notReally47@users.noreply.github.com>
2022-10-28 14:24:04 +0800

    v2.1
    Removed unnecessary functions

--- Inc/utils.h
+++ Inc/utils.h
@@ -4,10 +4,6 @@
 
 int IsAreaClicked(float area_center_x, float area_center_y, float area_width, float area_height, float click_x, float click_y);
 
-int IsCircleClicked(float circle_center_x, float circle_center_y, float diameter, float click_x, float click_y);
-
-union CP_Vector AngleToVector(float radian_angle);
-
 int getDirection(void);
 
 void getCell(int posX, int posY, int direction, Cell grid[SOKOBAN_GRID_ROWS][SOKOBAN_GRID_COLS]);
