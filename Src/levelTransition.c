d6860699f4e17ab6c9fb065c7fb5747025fb501e
notReally47 <notreally47@gmail.com>
2022-11-06 19:13:30 +0800

    Update levelTransition.c
    integrated card_selection() from mechanics

--- Src/levelTransition.c
+++ Src/levelTransition.c
@@ -1,8 +1,10 @@
-#include "cprocessing.h"	//Needed for C Processing Functions
-#include "utils.h"			//Needed for Mouse Click Logic
+#include "cprocessing.h"	// Needed for C Processing Functions
+#include "utils.h"			// Needed for Mouse Click Logic
 #include "defines.h"		// Needed for define Values
 #include "mainmenu.h"		// Needed to Return to Main Menu
 #include "basegame.h"		// Needed to Transit to Next Level
+#include "mechanics.h"		// Needed for card_selection()
+#include "levellogic.h"		// Needed for level global variable
 
 extern Config config;
 float windowwidth, windowheight;
@@ -28,7 +30,29 @@ void Level_Transition_Init()
 void Level_Transition_Update()
 {
 	CP_Graphics_ClearBackground(GRAY);
-	CP_Settings_Fill(RED); // Fill Rectangle RED
+	switch (level){
+    case 4:
+	case 8:
+	case 12:
+		CP_Settings_Fill(GRAY); // Fill Rectangle GRAY
+		card_selection(1,CP_Input_MouseClicked());
+		break;
+	case 6:
+	case 10:
+	case 14:
+		CP_Settings_Fill(GRAY); // Fill Rectangle GRAY
+		card_selection(0,CP_Input_MouseClicked());
+		break;
+	default:
+		CP_Settings_Fill(RED); // Fill Rectangle RED
+		if (CP_Input_MouseClicked()) { // Check for Mouse Input if Clicked, then checks if the Mouse is within any of the Rectangles
+			if (IsAreaClicked(buttons.center_x, buttons.center_y, buttons.width, buttons.height, CP_Input_GetMouseX(), CP_Input_GetMouseY()))
+				CP_Engine_SetNextGameState(base_Init, base_Update, base_Exit); // Go to the Next Level
+			else if (IsAreaClicked(buttons.center_x, buttons.center_y + buttons.height * 1.5f, buttons.width, buttons.height, CP_Input_GetMouseX(), CP_Input_GetMouseY()))
+				CP_Engine_SetNextGameState(Main_Menu_Init, Main_Menu_Update, Main_Menu_Exit); // Return to Main Menu
+		}
+		break;
+	}
 	CP_Graphics_DrawRect(buttons.center_x, buttons.center_y, buttons.width, buttons.height); // Draw 1st Button as 'Next'
 	CP_Graphics_DrawRect(buttons.center_x, buttons.center_y + buttons.height * 1.5f, buttons.width, buttons.height); // Draw 2nd Button as 'Main Menu'
 	CP_Settings_Fill(BLACK); // Set font to BLACK
@@ -43,15 +67,6 @@ void Level_Transition_Update()
 	CP_Settings_TextSize(100.f);
 	CP_Font_DrawText("Level Complete!", (float)CP_System_GetWindowWidth() / 2, 150.f);
 
-	// Check for Mouse Input if Clicked, then checks if the Mouse is within any of the Rectangles
-	if (CP_Input_MouseClicked()) { 
-		if (IsAreaClicked(buttons.center_x, buttons.center_y, buttons.width, buttons.height, CP_Input_GetMouseX(), CP_Input_GetMouseY())) {
-			CP_Engine_SetNextGameState(base_Init, base_Update, base_Exit); // Go to the Next Level
-		}
-		else if (IsAreaClicked(buttons.center_x, buttons.center_y + buttons.height * 1.5f, buttons.width, buttons.height, CP_Input_GetMouseX(), CP_Input_GetMouseY())) {
-			CP_Engine_SetNextGameState(Main_Menu_Init, Main_Menu_Update, Main_Menu_Exit); // Return to Main Menu
-		}
-	}
 }
 
 void Level_Transition_Exit()
