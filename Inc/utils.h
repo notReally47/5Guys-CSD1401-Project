3968cead0eb36cc132ed823497876ccb1c146ff7
notReally47 <notreally47@gmail.com>
2022-11-06 19:12:33 +0800

    Update utils.h
    swapped posX/Y with pointers

--- Inc/utils.h
+++ Inc/utils.h
@@ -7,7 +7,7 @@ extern float lockTimer;
 
 int IsAreaClicked(float area_center_x, float area_center_y, float area_width, float area_height, float click_x, float click_y);
 int getDirection(void);
-void getCell(int posX, int posY, int direction, Cell grid[SOKOBAN_GRID_ROWS][SOKOBAN_GRID_COLS]);
-void gameLogic(int posX, int posY, int nextPosX, int nextPosY, int prevPosX, int prevPosY, Cell grid[SOKOBAN_GRID_ROWS][SOKOBAN_GRID_COLS]);
+void getCell(int *posX, int *posY, int direction, Cell grid[SOKOBAN_GRID_ROWS][SOKOBAN_GRID_COLS]);
+void gameLogic(int *posX, int *posY, int nextPosX, int nextPosY, int prevPosX, int prevPosY, Cell grid[SOKOBAN_GRID_ROWS][SOKOBAN_GRID_COLS]);
 int timer(int duration, float startTime);
 int getObjective(Cell grid[SOKOBAN_GRID_ROWS][SOKOBAN_GRID_COLS]);
