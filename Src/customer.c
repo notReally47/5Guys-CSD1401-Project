c90f87d699db8f9ef71d6ae6ce1c633ba427cfe5
FyreWhale <tan.jianyu.jerell@gmail.com>
2022-11-08 11:34:09 +0800

    Clarify the variable naming in customer.c Changed posX to cusRow Changed posY to cusCol

--- Src/customer.c
+++ Src/customer.c
@@ -14,27 +14,27 @@
 * Cell grid: Grid where the customer exists.
 * Customer customer: Customer stats.
 */
-void customerLogic(int cusNum, int posX, int posY, int prevPosX, int prevPosY, int direction, Cell grid[SOKOBAN_GRID_ROWS][SOKOBAN_GRID_COLS], Customer customer[CUSTOMER_MAX]) {
-	if (!grid[posX][posY].shelf && !grid[posX][posY].boarder && !grid[posX][posY].box && !grid[posX][posY].customer && !grid[posX][posY].key && !grid[posX][posY].player &&!grid[posX][posY].mecha) {
+void customerLogic(int cusNum, int cusRow, int cusCol, int prevCusRow, int prevCusCol, int direction, Cell grid[SOKOBAN_GRID_ROWS][SOKOBAN_GRID_COLS], Customer customer[CUSTOMER_MAX]) {
+	if (!grid[cusRow][cusCol].shelf && !grid[cusRow][cusCol].boarder && !grid[cusRow][cusCol].box && !grid[cusRow][cusCol].customer && !grid[cusRow][cusCol].key && !grid[cusRow][cusCol].player &&!grid[cusRow][cusCol].mecha) {
 		if (customer[cusNum].isRandom) {
 			/*Limit customer random movement within 5x5 grid*/
-			if ((customer[cusNum].ogCusCol - 2 <= posY && posY <= customer[cusNum].ogCusCol + 2) &&
-				(customer[cusNum].ogCusRow - 2 <= posX && posX <= customer[cusNum].ogCusRow + 2)) {
-				grid[posX][posY].customer = 1;
-				grid[prevPosX][prevPosY].customer = 0;
+			if ((customer[cusNum].ogCusCol - 2 <= cusCol && cusCol <= customer[cusNum].ogCusCol + 2) &&
+				(customer[cusNum].ogCusRow - 2 <= cusRow && cusRow <= customer[cusNum].ogCusRow + 2)) {
+				grid[cusRow][cusCol].customer = 1;
+				grid[prevCusRow][prevCusCol].customer = 0;
 
-				customer[cusNum].cusRow = posX;
-				customer[cusNum].cusCol = posY;
+				customer[cusNum].cusRow = cusRow;
+				customer[cusNum].cusCol = cusCol;
 				customer[cusNum].direction = direction;
 			}
 		}
 
 		else {
-			grid[posX][posY].customer = 1;
-			grid[prevPosX][prevPosY].customer = 0;
+			grid[cusRow][cusCol].customer = 1;
+			grid[prevCusRow][prevCusCol].customer = 0;
 
-			customer[cusNum].cusRow = posX;
-			customer[cusNum].cusCol = posY;
+			customer[cusNum].cusRow = cusRow;
+			customer[cusNum].cusCol = cusCol;
 			customer[cusNum].direction = direction;
 		}
 	}
@@ -49,9 +49,9 @@ void customerLogic(int cusNum, int posX, int posY, int prevPosX, int prevPosY, i
 void customerMovement(Cell grid[SOKOBAN_GRID_ROWS][SOKOBAN_GRID_COLS], int path[SOKOBAN_GRID_ROWS][SOKOBAN_GRID_COLS], Customer customer[CUSTOMER_MAX]) {
 	for (int i = 0; i < CUSTOMER_MAX; i++) {
 		if (customer[i].isActive && !customer[i].isIdle && !customer[i].isRandom) {
-			int posX = customer[i].cusRow;
-			int posY = customer[i].cusCol;
-			int curr = path[posX][posY];
+			int cusRow = customer[i].cusRow;
+			int cusCol = customer[i].cusCol;
+			int curr = path[cusRow][cusCol];
 
 			int count = CP_System_GetFrameCount();
 
@@ -66,23 +66,23 @@ void customerMovement(Cell grid[SOKOBAN_GRID_ROWS][SOKOBAN_GRID_COLS], int path[
 				switch (curr) {
 					// Move Up
 				case SOKOBAN_UP:
-					posX--;
-					customerLogic(i, posX, posY, posX + 1, posY, curr, grid, customer);
+					cusRow--;
+					customerLogic(i, cusRow, cusCol, cusRow + 1, cusCol, curr, grid, customer);
 					break;
 					// Move Left
 				case SOKOBAN_LEFT:
-					posY--;
-					customerLogic(i, posX, posY, posX, posY + 1, curr, grid, customer);
+					cusCol--;
+					customerLogic(i, cusRow, cusCol, cusRow, cusCol + 1, curr, grid, customer);
 					break;
 					// Move Down
 				case SOKOBAN_DOWN:
-					posX++;
-					customerLogic(i, posX, posY, posX - 1, posY, curr, grid, customer);
+					cusRow++;
+					customerLogic(i, cusRow, cusCol, cusRow - 1, cusCol, curr, grid, customer);
 					break;
 					// Move Right
 				case SOKOBAN_RIGHT:
-					posY++;
-					customerLogic(i, posX, posY, posX, posY - 1, curr, grid, customer);
+					cusCol++;
+					customerLogic(i, cusRow, cusCol, cusRow, cusCol - 1, curr, grid, customer);
 					break;
 					// Default case (if any)
 				default:
@@ -101,25 +101,25 @@ void customerMovement(Cell grid[SOKOBAN_GRID_ROWS][SOKOBAN_GRID_COLS], int path[
 void randomCustomerMovement(Cell grid[SOKOBAN_GRID_ROWS][SOKOBAN_GRID_COLS], Customer customer[CUSTOMER_MAX]) {
 	for (int i = 0; i < CUSTOMER_MAX; i++) {
 		if (customer[i].isActive && customer[i].isRandom) {
-			int posX = customer[i].cusRow, posY = customer[i].cusCol;
+			int cusRow = customer[i].cusRow, cusCol = customer[i].cusCol;
 			int state = CP_Random_RangeInt(SOKOBAN_UP, SOKOBAN_FACE_RIGHT);
 			if (!(CP_System_GetFrameCount() % CUSTOMER_RANDOM_SPEED)) {
 				switch (state) {
 				case SOKOBAN_UP:
-					posX--;
-					customerLogic(i, posX, posY, posX + 1, posY, SOKOBAN_UP, grid, customer);
+					cusRow--;
+					customerLogic(i, cusRow, cusCol, cusRow + 1, cusCol, SOKOBAN_UP, grid, customer);
 					break;
 				case SOKOBAN_LEFT:
-					posY--;
-					customerLogic(i, posX, posY, posX, posY + 1, SOKOBAN_LEFT, grid, customer);
+					cusCol--;
+					customerLogic(i, cusRow, cusCol, cusRow, cusCol + 1, SOKOBAN_LEFT, grid, customer);
 					break;
 				case SOKOBAN_DOWN:
-					posX++;
-					customerLogic(i, posX, posY, posX - 1, posY, SOKOBAN_DOWN, grid, customer);
+					cusRow++;
+					customerLogic(i, cusRow, cusCol, cusRow - 1, cusCol, SOKOBAN_DOWN, grid, customer);
 					break;
 				case SOKOBAN_RIGHT:
-					posY++;
-					customerLogic(i, posX, posY, posX, posY - 1, SOKOBAN_RIGHT, grid, customer);
+					cusCol++;
+					customerLogic(i, cusRow, cusCol, cusRow, cusCol - 1, SOKOBAN_RIGHT, grid, customer);
 					break;
 				case SOKOBAN_FACE_UP:
 					customer[i].direction = SOKOBAN_UP;
@@ -165,15 +165,17 @@ int customerLock(Cell grid[SOKOBAN_GRID_ROWS][SOKOBAN_GRID_COLS], Customer custo
 	int isLocked = 0;
 	for (int i = 0; i < CUSTOMER_MAX; i++) {
 		if (customer[i].isActive) {
+			int cusRow = customer[i].cusRow;
+			int cusCol = customer[i].cusCol;
 			switch (customer[i].direction) {
 				/* Face up */
 			case SOKOBAN_UP:
 				for (int x = 1; x <= customer[i].range; x++) {
-					if (grid[customer[i].cusRow - x][customer[i].cusCol].box || grid[customer[i].cusRow - x][customer[i].cusCol].shelf) {
+					if (grid[cusRow - x][cusCol].box || grid[cusRow - x][cusCol].shelf) {
 						break;
 					}
 
-					if (grid[customer[i].cusRow - x][customer[i].cusCol].player) {
+					if (grid[cusRow - x][cusCol].player) {
 						isLocked = 1;
 						customer[i].isActive = 0;
 						if (infected[i] == 1) // from mechanics.h
@@ -187,11 +189,11 @@ int customerLock(Cell grid[SOKOBAN_GRID_ROWS][SOKOBAN_GRID_COLS], Customer custo
 				/* Face left */
 			case SOKOBAN_LEFT:
 				for (int x = 1; x <= customer[i].range; x++) {
-					if (grid[customer[i].cusRow][customer[i].cusCol - x].box || grid[customer[i].cusRow][customer[i].cusCol -x].shelf) {
+					if (grid[cusRow][cusCol - x].box || grid[cusRow][cusCol -x].shelf) {
 						break;
 					}
 
-					if (grid[customer[i].cusRow][customer[i].cusCol - x].player) {
+					if (grid[cusRow][cusCol - x].player) {
 						isLocked = 1;
 						customer[i].isActive = 0;
 						if (infected[i] == 1) // from mechanics.h
@@ -204,11 +206,11 @@ int customerLock(Cell grid[SOKOBAN_GRID_ROWS][SOKOBAN_GRID_COLS], Customer custo
 				/* Face down */
 			case SOKOBAN_DOWN:
 				for (int x = 1; x <= customer[i].range; x++) {
-					if (grid[customer[i].cusRow + x][customer[i].cusCol].box || grid[customer[i].cusRow + x][customer[i].cusCol].shelf) {
+					if (grid[cusRow + x][cusCol].box || grid[cusRow + x][cusCol].shelf) {
 						break;
 					}
 
-					if (grid[customer[i].cusRow + x][customer[i].cusCol].player) {
+					if (grid[cusRow + x][cusCol].player) {
 						isLocked = 1;
 						customer[i].isActive = 0;
 						if (infected[i] == 1) // from mechanics.h
@@ -221,11 +223,11 @@ int customerLock(Cell grid[SOKOBAN_GRID_ROWS][SOKOBAN_GRID_COLS], Customer custo
 				/* Face right */
 			case SOKOBAN_RIGHT:
 				for (int x = 1; x <= customer[i].range; x++) {
-					if (grid[customer[i].cusRow][customer[i].cusCol + x].box || grid[customer[i].cusRow][customer[i].cusCol + x].shelf) {
+					if (grid[cusRow][cusCol + x].box || grid[cusRow][cusCol + x].shelf) {
 						break;
 					}
 
-					if (grid[customer[i].cusRow][customer[i].cusCol + x].player) {
+					if (grid[cusRow][cusCol + x].player) {
 						isLocked = 1;
 						customer[i].isActive = 0;
 						if (infected[i] == 1) // from mechanics.h
