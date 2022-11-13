26aa93e59630ff57c52ffda4b5052a72de996606
ilovebnc <guoyiming340@gmail.com>
2022-11-13 20:10:41 +0800

    changed project properties>debugging>working directory so assets willload in release

--- Inc/options_draw.h
+++ Inc/options_draw.h
@@ -68,7 +68,8 @@ void drawAlignedText(CP_Color color, int alignment, const char* text, float x, f
 * GIF* gif: Pointer of gif created
 * float* timeElapsed: Pointer to time elapsed in game
 * const float displayDuration: Duration each frame to be displayed in seconds
-* Flag flip: Flag to determine whether to flip the left and right of the gif
+* Flag flipLR: Flag to determine whether to flip the left and right of the gif
+* Flag flipTB: Flag to determine whether to flip the top and bottom of the gif
 */
 void drawGIF(GIF* gif, float* timeElapsed, const float displayDuration, Flag flipLR, Flag flipTB);
 
