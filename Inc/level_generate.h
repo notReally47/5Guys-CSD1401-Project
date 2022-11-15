c44aab3e12faf8dd6e8dd233d2066332228d40af
Faliqoes <faliqhakim@outlook.com>
2022-11-16 02:55:19 +0800

    Added Comments

--- Inc/level_generate.h
+++ Inc/level_generate.h
@@ -3,4 +3,5 @@
 #include "defines.h"
 #include "structs.h"
 
-void setMap(Cell grid[SOKOBAN_GRID_ROWS][SOKOBAN_GRID_COLS], Customer customer[CUSTOMER_MAX], int path[SOKOBAN_GRID_ROWS][SOKOBAN_GRID_COLS], Teleporter teleporters[TELEPORTER_NUMBER]);
+void set_map(Cell grid[SOKOBAN_GRID_ROWS][SOKOBAN_GRID_COLS], Customer customer[CUSTOMER_MAX],
+	int path[SOKOBAN_GRID_ROWS][SOKOBAN_GRID_COLS], Teleporter teleporters[TELEPORTER_NUMBER]);
--- Inc/structs.h
+++ Inc/structs.h
@@ -8,10 +8,10 @@ typedef struct Save {
 } Save;
 
 typedef struct Settings {
-	int				audio; /*0 - 100*/
-	int				windowed; /*windowed: 1, fullscreen: 0*/
-	unsigned int	resolutionWidth; /*960, 1920*/
-	unsigned int	resolutionHeight; /*540, 1080*/
+	int				audio;					// 0 - 100
+	int				windowed;				// windowed: 1, fullscreen: 0
+	unsigned int	resolutionWidth;		// 960, 1920
+	unsigned int	resolutionHeight;		// 540, 1080
 } Settings;
 
 typedef struct UniqueCards {		// struct to be saved in config.dat
@@ -30,40 +30,40 @@ typedef struct Config {
 } Config;
 
 typedef struct Customer {
-	/*Current position*/
-	int				cusRow;
-	int				cusCol;
-	/*Original position*/
-	int				ogCusRow;
-	int				ogCusCol;
 
-	int				direction;
-	int				range; /*Detection range*/
-	int				isActive; /*Ability to stun player.*/
-	int				isIdle;
-	int				isRandom;
+	int				cusRow;					// Current Customer Row
+	int				cusCol;					// Current Customer Column
+
+	int				ogCusRow;				// Original Customer Row
+	int				ogCusCol;				// Original Customer Column
+
+	int				direction;				// Where Customer is facing
+	int				range;					// Detection range, Current Default is 2
+	int				isActive;				// Ability to stun player
+	int				isIdle;					// Idle or not
+	int				isRandom;				// Path is random or not
 } Customer;
 
 typedef struct Cell {
-	int				player;
-	int				key;
-	int				box;
-	int				tele;
-	int				boarder;
-	int				customer;
-	int				shelf;
-	int				mecha;
+	int				player;					// Flag for Player, 1 is Player / 0 is Not Player
+	int				key;					// Key Number, 0 if does not exist / Also Flag for Key, !0 is Key / 0 is not Key
+	int				box;					// Box Number, 0 if does not exist / Also Flag for Box, !0 is Box / 0 is not Box
+	int				tele;					// Teleporter Number, 0 if does not exist / Also Flag for Teleporter, !0 is Teleporter / 0 is not Teleporter
+	int				boarder;				// Flag for Boarder, 1 is Boarder/ 0 is not Boarder
+	int				customer;				// Customer Number, 0 if does not exist / Also Flag for Customer, !0 is Customer/ 0 is not Customer
+	int				shelf;					// Flag for Shelf, 1 is Shelf / 0 is not Shelf
+	int				mecha;					// Flag for Mechanics, 1 is Mechanics / 0 is not Mechanics
 } Cell;
 
 typedef struct Move {
-	int				player;
-	int				box;
+	int				player;					// To save Player Position
+	int				box;					// To save Box Poistions
 } Move;
 
 typedef struct Teleporter {
-	int				teleporter_number;
-	int				posY;
-	int				posX;
+	int				teleporter_number;		// Teleporter Number
+	int				posY;					// Teleporter Row
+	int				posX;					// Teeleporter Column
 } Teleporter;
 
 typedef struct rect {
@@ -102,8 +102,8 @@ typedef struct GIF {
 } GIF;
 
 typedef struct CardPosition {
-	int				neg1;	// negative card position of left card
-	int				neg2;	// negative card position of right card
-	int				pos1;	// positive card position of left card
-	int				pos2;	// positive card position of right card
-} CardPosition;	// position is varied from 0 to 5 for the cards array
+	int				neg1;			// negative card position of left card
+	int				neg2;			// negative card position of right card
+	int				pos1;			// positive card position of left card
+	int				pos2;			// positive card position of right card
+} CardPosition;						// position is varied from 0 to 5 for the cards array
